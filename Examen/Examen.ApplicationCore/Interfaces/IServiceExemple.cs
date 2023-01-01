05236f12002e370e56403be362d9b0cd043c8f82
mathlouthi dhia <dhia.mathlouthi@esprit.tn>
2023-01-01 16:02:49 +0100

    first commit

---
+++ Examen/Examen.ApplicationCore/Interfaces/IServiceExemple.cs
@@ -0,0 +1,14 @@
+﻿using Examen.ApplicationCore.Domain;
+using Examen.ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen.ApplicationCore.Interfaces
+{
+    public interface IServiceExemple: IService<Exemple>
+    {
+    }
+}
---
+++ Examen/Examen.Infrastructure/ExamenContext.cs
@@ -0,0 +1,28 @@
+﻿using Examen.ApplicationCore.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Examen.Infrastructure
+{
+    public class ExamenContext: DbContext
+    {
+        public DbSet<Exemple> Exemples { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;
+              Initial Catalog=ExamenDB;Integrated Security=true");
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+        }
+
+    }
+}
